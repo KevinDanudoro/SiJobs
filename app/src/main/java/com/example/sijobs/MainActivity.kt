16d7bb286d3fe9d12cb92f8589453d1dcc26cd42
KevinDanudoro <kevindanudoro68@gmail.com>
2022-03-06 22:31:07 +0700

    Fitur Intent
    Penambahan fitur intent pada login page ke new profile

--- app/src/main/java/com/example/sijobs/MainActivity.kt
+++ app/src/main/java/com/example/sijobs/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.example.sijobs
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnlogin : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnlogin = findViewById(R.id.button)
+        btnlogin.setOnClickListener {
+            Intent(this, NewProfileActivity::class.java).also{
+                startActivity(it)
+            }
+        }
+
     }
 }
