1c60330544e1f97bce7367b7a45812d824a5b9da
dzikryaji <dzikryaji07@gmail.com>
2022-03-12 14:51:12 +0700

    add bottom nav and some fragments

---
+++ app/src/main/java/com/example/sijobs/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.sijobs
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.sijobs.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val homeFragment = HomeFragment()
+        val searchFragment = SearchFragment()
+        val jobFragment = JobFragment()
+        val chatFragment = ChatFragment()
+        val profileFragment = ProfileFragment()
+
+        setCurrentFragment(homeFragment)
+
+        binding.bottomNavigationView.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.miHome -> setCurrentFragment(homeFragment)
+                R.id.miSearch -> setCurrentFragment(searchFragment)
+                R.id.miJob -> setCurrentFragment(jobFragment)
+                R.id.miChat -> setCurrentFragment(chatFragment)
+                R.id.miProfile -> setCurrentFragment(profileFragment)
+            }
+            true
+        }
+    }
+
+    //function to set fragment
+    private fun setCurrentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fl_Fragmment, fragment)
+            commit()
+        }
+}
