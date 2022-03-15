9438ec36c99e3321ab820e47258c5b937c9bca94
dzikryaji <dzikryaji07@gmail.com>
2022-03-15 19:32:16 +0700

    home fragment tapi blom bener recycleviewnya

---
+++ app/src/main/java/com/example/sijobs/HomeHistory.kt
@@ -0,0 +1,8 @@
+package com.example.sijobs
+
+
+data class HomeHistory (
+    val name: String,
+    val job: String,
+    val date: String,
+)
---
+++ app/src/main/java/com/example/sijobs/HomeHistoryAdapter.kt
@@ -0,0 +1,29 @@
+package com.example.sijobs
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.sijobs.databinding.ItemHomehistoryBinding
+
+class HomeHistoryAdapter(var histories: List<HomeHistory>): RecyclerView.Adapter<HomeHistoryAdapter.HistoryViewHolder>()  {
+
+    inner class HistoryViewHolder(val binding: ItemHomehistoryBinding) : RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HistoryViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding = ItemHomehistoryBinding.inflate(layoutInflater, parent, false)
+        return HistoryViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: HistoryViewHolder, position: Int) {
+        holder.binding.apply {
+            tvNamehistory.text = histories[position].name
+            tvHisJob.text = histories[position].job
+            tvTime.text = histories[position].date
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return histories.size
+    }
+}
