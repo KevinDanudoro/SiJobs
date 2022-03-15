791744f5ab53c28823c540dbf677685e81882f3a
dzikryaji <dzikryaji07@gmail.com>
2022-03-15 20:23:49 +0700

    home fragment kodingan kotlinnya diganti

--- app/src/main/java/com/example/sijobs/HomeFragment.kt
+++ app/src/main/java/com/example/sijobs/HomeFragment.kt
@@ -34,7 +34,7 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_job, container, false)
+        return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
     companion object {
@@ -49,7 +49,7 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            JobFragment().apply {
+            HomeFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
